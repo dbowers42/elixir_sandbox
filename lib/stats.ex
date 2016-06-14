ddbde505dfa578396d3295811f19ba8ec489ef9c
dbowers42 <dbowers.github@gmail.com>
2016-06-14 13:43:00 -0600

    move status functions in to lib

---
+++ lib/stats.ex
@@ -0,0 +1,39 @@
+require Integer
+
+defmodule Program do
+  def mean(list) do
+    Enum.sum(list) / Enum.count(list)
+  end
+
+  def median(list) do
+    sorted = list |> Enum.sort
+
+    index = sorted
+    |> Enum.count
+    |> div(2)
+
+    cond do
+      Enum.count(sorted) |> Integer.is_even ->
+        mean([Enum.at(sorted, index), Enum.at(sorted, index - 1)])
+      true ->
+        Enum.at(sorted, index)
+    end
+  end
+
+  def mode(list) do
+    grouped = list
+    |> Enum.group_by(&(&1))
+    |> Enum.map(fn {key, value} -> {key, Enum.count(value)} end)
+
+
+
+    max = grouped
+    |> Enum.max_by(fn {k, v} -> v end)
+    |> elem(1)
+
+    grouped
+    |> Enum.filter(fn {k, v} -> v == max end)
+    |> Enum.map(fn {k, v} -> k end)
+
+  end
+end
--- program.ex
+++ program.ex
@@ -1,36 +1,18 @@
-require Integer
-
 defmodule Program do
-  def mean(list) do
-    Enum.sum(list) / Enum.count(list)
-  end
-
-  def median(list) do
-    sorted = list |> Enum.sort
-
-    index = sorted
-    |> Enum.count
-    |> div(2)
-
-    cond do
-      Enum.count(sorted) |> Integer.is_even ->
-        mean([Enum.at(sorted, index), Enum.at(sorted, index - 1)])
-      true ->
-        Enum.at(sorted, index)
+  def foo do
+    receive do
+      {message} ->
+        IO.puts message
+      foo
     end
   end
 
-  def mode(list) do
-    list
-    |> Enum.sort
-    |> Enum.group_by(&(&1))
-    |> Enum.map fn {key, value} -> {key, Enum.count(value)} end
-    |> Enum.sort_by fn {_key, c} -> c end
+  def foobar do
+    pid = spawn(Program, :foo, [])
+    Process.register(pid, :msg)
+
   end
 end
 
-a = [1,1,3,4, 5,5, 5]
-
-a |> Program.mean() |> IO.inspect
-a |> Program.median() |> IO.inspect
-a |> Program.mode() |> IO.inspect
+Program.foobar
+send(:msg, {"hi"})
